b8e37c42bb1b06b2303a7c15de5408736fec4c1f
fiestyfondue <84001951+fiestyfondue@users.noreply.github.com>
2021-06-10 14:03:11 +0530

    [Harsh]Add:-New method GambleGame is added

--- Program.cs
+++ Program.cs
@@ -6,15 +6,29 @@ namespace Gamble
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Gambling Game");
-            int Stake = 100;
-            int bet = 1;
-            int Noofdays = 100;
+            Console.WriteLine("Welcome to Gamble Game");
+            //const int STAKE = 100;
+            //const int BET = 1;
+            GambleGame();
 
-            Console.Write("The gambler has " + Stake + "with him and he is ready to bet " + bet + "EveryDay for " + Noofdays + "days");
-            Console.ReadLine();
         }
-            
+        public static void GambleGame()
+        {
+            const int WIN = 0;
+            const int LOSE = 1;
+            Random random = new Random();
+            int game = random.Next(0, 2);
+            if (game == WIN)
+            {
+                Console.WriteLine("gambler wins!");
+            }
+            if (game == LOSE)
+            {
+                Console.WriteLine("gambler lose!");
+            }
+
+        }
+
     }
 }
 
