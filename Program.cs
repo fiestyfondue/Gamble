2f3841eda625f6c3c14b0a142ca666db9858a2b9
fiestyfondue <84001951+fiestyfondue@users.noreply.github.com>
2021-06-10 13:20:24 +0530

    [Harsh]Add:-new program gamble is added

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Gamble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Gambling Game");
+            int Stake = 100;
+            int bet = 1;
+            int Noofdays = 100;
+
+            Console.Write("The gambler has " + Stake + "with him and he is ready to bet " + bet + "EveryDay for " + Noofdays + "days");
+            Console.ReadLine();
+        }
+            
+    }
+}
+
